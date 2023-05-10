d92d9209674bed05d371881f58dcc06aa59016e6
Vodu <VoduMapping@gmail.com>
2023-05-10 16:52:04 +0200

    Sehr Grundlegende darstellung eines einzelnen Neuronen als Klasse

--- Neural-Network/neuron.py
+++ Neural-Network/neuron.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 import numpy as np 
 
 class neuron():
-    """Basicly this represents a single neuron, wich basicly outputs a vector"""
+    """Ein Neuron, had eine liste von eingaben, und eine liste von gewichten und eine Voreingenommenheit. Dammit rechnen wir dann denn ausgangs wert."""
     def __init__(self,inputs: list, weights: list,bias:float) -> None:
         self.inputs = inputs
         self.weights = weights
@@ -10,6 +10,9 @@ class neuron():
         self.bias = bias
     
     def calc_Output(self):
+        """Matrix Multiplikation, das heist für alle eingabe werte die existieren, rechne Eingabe * Gewichtung + Voreingenommenheit. Dies wird danach in eine sigmoid funktion gebracht(Akitvations Funktion)
+        """
         for input,weight in zip(self.inputs,self.weights):
             self.output += input * weight + self.bias
+        #Sigmoid Funktion
         self.output = 1/(1+pow(2.718281828459045,-self.output))
