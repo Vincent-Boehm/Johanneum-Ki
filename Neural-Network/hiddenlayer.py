6a9eceb543cb51a018048cacbb68982dd2e9441f
Vincent Böhm <Vincent.Boehm2004@gmail.com>
2023-05-14 19:03:52 +0200

    commented out everything that is unessecary for now

--- Neural-Network/hiddenlayer.py
+++ Neural-Network/hiddenlayer.py
@@ -1,27 +1,27 @@
-import hiddenNeuron
+# import hiddenNeuron
 
-class hiddenlayer():
-    """Beschriebt eine "Schicht des Neuralen Netzwerks"
-    """
-    def __init__(self,depth,neuron_number,weight_list,bias_list) -> None:
-        """_summary_
+# class hiddenlayer():
+#     """Beschriebt eine "Schicht des Neuralen Netzwerks"
+#     """
+#     def __init__(self,depth,neuron_number,weight_list,bias_list) -> None:
+#         """_summary_
 
-        Args:
-            depth (int): Wie "tief" sich diese Schicht befindet, das heißt wie weit es von denn eingangs neuronen entfernt ist.
-            neuron_number (int): Wie viele neuronen sich in dieser schicht befinden sollen.
-            input_list (list): Liste der Listen für die Eingaben der Neuronen.
-            weight_list (list): Liste der Listen für die Eingaben der Gewichtungen.
-        """
-        self.depth:int = depth
-        self.neuron_number:int = neuron_number
-        self.neurons = []
+#         Args:
+#             depth (int): Wie "tief" sich diese Schicht befindet, das heißt wie weit es von denn eingangs neuronen entfernt ist.
+#             neuron_number (int): Wie viele neuronen sich in dieser schicht befinden sollen.
+#             input_list (list): Liste der Listen für die Eingaben der Neuronen.
+#             weight_list (list): Liste der Listen für die Eingaben der Gewichtungen.
+#         """
+#         self.depth:int = depth
+#         self.neuron_number:int = neuron_number
+#         self.neurons = []
         
-        self.input_list = []
+#         self.input_list = []
         
-        self.weight_list = weight_list
-        self.bias_list = bias_list
+#         self.weight_list = weight_list
+#         self.bias_list = bias_list
         
         
-        #Generiert Neuronen
-        for number in range(self.neuron_number):
-            self.neurons.append(hiddenNeuron.neuron(self.input_list[number],self.weight_list[number],self.bias_list[number]))
+#         #Generiert Neuronen
+#         for number in range(self.neuron_number):
+#             self.neurons.append(hiddenNeuron.neuron(self.input_list[number],self.weight_list[number],self.bias_list[number]))
--- Neural-Network/network.py
+++ Neural-Network/network.py
@@ -1,15 +1,15 @@
-import hiddenLayer
+# import hiddenLayer
 
-import random
+# import random
 
-class neuralNetwork():
-    def __init__(self,hiddenLayers,hiddenLayerSize,inputLayer,outputLayer) -> None:
-        self.hiddenLayers = hiddenLayers
-        self.hiddenLayerSize = hiddenLayerSize
-        self.inputLayer = inputLayer
-        self.outputLayer = outputLayer
-        
-        
-        self.layers = []
-        for layers in self.hiddenLayers:
-            self.layers.append(hiddenLayer.hiddenlayer(layers,self.hiddenLayerSize,0,random.randint(10,size=(hiddenLayerSize))))
+# class neuralNetwork():
+#     def __init__(self,hiddenLayers,hiddenLayerSize,inputLayer,outputLayer) -> None:
+#         self.hiddenLayers = hiddenLayers
+#         self.hiddenLayerSize = hiddenLayerSize
+#         self.inputLayer = inputLayer
+#         self.outputLayer = outputLayer
+
+
+#         self.layers = []
+#         for layers in self.hiddenLayers:
+#             self.layers.append(hiddenLayer.hiddenlayer(layers,self.hiddenLayerSize,0,random.randint(10,size=(hiddenLayerSize))))
