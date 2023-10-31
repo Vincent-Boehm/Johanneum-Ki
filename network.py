64cc83f5c027c8d045b034504a779724a2c0dd6f
Vincent Böhm <Vincent.Boehm2004@gmail.com>
2023-10-31 12:59:39 +0100

    troll and aron

--- network.py
+++ network.py
@@ -2,32 +2,41 @@ import numpy as np
 
 class network:
     def __init__(self) -> None:
-        self.layers = 5
+        self.layers = 3
+        self.fitness = 0
         
-        self.weights1 = np.random.rand(18,10) + 0.5
-        self.weights2 = np.random.rand(18,10) + 0.5
-        self.weights3 = np.random.rand(18,10) + 0.5
-        self.weights4 = np.random.rand(18,10) + 0.5
-        self.weights5 = np.random.rand(18,10) + 0.5
+        self.weightsInput = np.random.uniform(-1,1,(10,180))
+        self.weights1 = np.random.uniform(-1,1,(10,10))
+        self.weights1 = np.random.uniform(-1,1,(10,10))
+        
         pass    
     
     def calculateOutput(self,input:np.ndarray) -> np.ndarray:
-        activationFunction = np.vectorize(lambda x: 1/1-np.e**-x)
+        #activationFunction = np.vectorize(lambda x: 1/1 + np.exp(-x))
         
+        def activationFunction(x:np.ndarray):
+            return (1/(1 + np.exp(-x)))
         
-        hiddenlayers = input * self.weights1
+        input = input.flatten()
+        
+        hiddenlayers = np.ndarray(10)
+        
+        for i in hiddenlayers:
+            hiddenlayers[i] = np.dot(input,self.weightsInput[:,i])
         hiddenlayers = activationFunction(hiddenlayers)
         
-        hiddenlayers = hiddenlayers * self.weights2
+        for i in hiddenlayers:
+            hiddenlayers[i] = np.dot(input,self.weights1[:,i])
+        hiddenlayers = activationFunction(hiddenlayers)      
+          
+        for i in hiddenlayers:
+            hiddenlayers[i] = np.dot(input,self.weights2[:,i])
         hiddenlayers = activationFunction(hiddenlayers)
+         
+        outputlayer = [0,0,0]
         
-        hiddenlayers = hiddenlayers * self.weights3
-        hiddenlayers = activationFunction(hiddenlayers)
+        outputlayer[0] = activationFunction(outputlayer[0])
+        outputlayer[1] = activationFunction(outputlayer[1])
+        outputlayer[2] = activationFunction(outputlayer[2])
         
-        hiddenlayers = hiddenlayers * self.weights4
-        hiddenlayers = activationFunction(hiddenlayers)
-        
-        hiddenlayers = hiddenlayers * self.weights5
-        hiddenlayers = activationFunction(hiddenlayers)
-        
-        return hiddenlayers
+        return outputlayer
--- tetris.py
+++ tetris.py
@@ -2,11 +2,16 @@ import os
 
 import sys
 
+import jsonpickle
+
 from pyboy import PyBoy, WindowEvent, botsupport
 
 import numpy as np
 
 import network
+
+#File name not code name
+networkName = "Jarvis.json"
 
 pyboy = PyBoy(gamerom_file="Tetris.gb",game_wrapper=True)
 pyboy.set_emulation_speed(0)
@@ -17,10 +22,21 @@ tetris = pyboy.game_wrapper()
 
 tetris.start_game(timer_div=0) 
 
+
+# Todo change it to only import the best one from the files and make the network from that
 neuralNetwork = network.network()
+
+learningRate = 1
 
 
 ticks:int = 0
+
+networkResults:np.ndarray = np.ones(3)
+
+networkResults[0] = 0
+networkResults[1] = 0
+networkResults[2] = 0
+
 while not pyboy.tick():
     ticks += 1
     
@@ -30,10 +46,35 @@ while not pyboy.tick():
         gameArea = np.array(tetris.game_area(),dtype=np.uint32)
         
         gameArea = (gameArea-np.min(gameArea))/(np.max(gameArea)-np.min(gameArea))
-
         
-        print(neuralNetwork.calculateOutput(gameArea))
+        networkResults = neuralNetwork.calculateOutput(gameArea)
         
         
         pass
     
+    pyboy.send_input(WindowEvent.RELEASE_ARROW_RIGHT)
+    pyboy.send_input(WindowEvent.RELEASE_ARROW_LEFT)
+    pyboy.send_input(WindowEvent.RELEASE_BUTTON_A)
+    
+    if networkResults[0] > 0.5:
+        pyboy.send_input(WindowEvent.PRESS_ARROW_RIGHT)
+    if networkResults[1] > 0.5:
+        pyboy.send_input(WindowEvent.PRESS_ARROW_LEFT)
+    if networkResults[2] > 0.5:
+        pyboy.send_input(WindowEvent.PRESS_BUTTON_A)
+    
+    if tetris.game_over():
+        neuralNetwork.fitness = tetris.fitness + tetris.level
+        file = jsonpickle.encode(neuralNetwork)
+        
+        with open(networkName,"w+") as outFile:
+            outFile.write(file)
+            
+            # neuralNetwork.weights1 = neuralNetwork.weights1 +  (np.random.uniform(-1,1,(18,10)) * learningRate)
+            # neuralNetwork.weights2 = neuralNetwork.weights2 +  (np.random.uniform(-1,1,(18,10)) * learningRate)
+            # neuralNetwork.weights3 = neuralNetwork.weights3 +  (np.random.uniform(-1,1,(18,10)) * learningRate)
+            
+            # neuralNetwork.outputLayerweights1 = neuralNetwork.outputLayerweights1 +  (np.random.uniform(-1,1,180) * learningRate)
+            # neuralNetwork.outputLayerweights2 = neuralNetwork.outputLayerweights2 +  (np.random.uniform(-1,1,180) * learningRate)
+            # neuralNetwork.outputLayerweights3 = neuralNetwork.outputLayerweights3 +  (np.random.uniform(-1,1,180) * learningRate)
+            tetris.reset_game(timer_div=0)
